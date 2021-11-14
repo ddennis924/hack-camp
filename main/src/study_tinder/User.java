9f5c8a7f4e7528eef8f23dbcc0cc282a51fa616e
Muhan Li <alanli8519@gmail.com>
2021-11-14 10:53:06 -0800

    Changes to user

--- main/src/study_tinder/User.java
+++ main/src/study_tinder/User.java
@@ -3,7 +3,6 @@ package study_tinder;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.List;
 
 public class User {
     private final String name;
--- main/src/study_tinder/user.java
+++ main/src/study_tinder/user.java
@@ -1,24 +1,38 @@
 package study_tinder;
 
-import java.util.ArrayList;
+
 import java.util.List;
+import java.util.ArrayList;
 
-public class user {
-    private String name;
-    private List<Question> questionList;
+public class User {
+    private final String name;
+    private final List<Question> questionList;
 
-    public user(String name){
+    public User(String name){
         this.name = name;
         this.questionList = new ArrayList<>();
     }
 
-    public user(String name, List<Question> seed){
+    public User(String name, List<Question> seed){
         this.name = name;
         this.questionList = seed;
     }
 
-    public user(List<Question> seed){
+    public User(List<Question> seed){
         this.name = "guest";
         this.questionList = seed;
     }
+
+    public void addQuestion(Question newQ){
+        questionList.add(newQ);
+    }
+
+    public List<Question> getQList(){
+        return new ArrayList<>(this.questionList);
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
 }
