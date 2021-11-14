2571720e13802eed4dbb33ad392e707f54973bfb
Muhan Li <alanli8519@gmail.com>
2021-11-14 11:05:14 -0800

    moved main to APP

--- main/src/study_tinder/App.java
+++ main/src/study_tinder/App.java
@@ -1,4 +1,12 @@
 package study_tinder;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class App {
+    public static void main(String[] args){
+        User muhan = new User("Alan");
+        muhan.addQuestion(new BufferedImage(20, 20 , BufferedImage.TYPE_BYTE_GRAY), Category.MATH );
+        System.out.println(muhan.getQList().size());
+    }
 }
